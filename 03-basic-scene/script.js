8b8886aed0dad71882cd60982dca1ee386040238
Pavel Klochkov <paul.klochkov@gmail.com>
2021-11-28 11:44:42 +0100

    Finish basic scene

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,3 +1,11 @@
+const canvas = document.querySelector("#webgl");
+
+// Sizes
+const SIZES = {
+  width: 800,
+  height: 600,
+};
+
 // Init scene
 const scene = new THREE.Scene();
 
@@ -7,7 +15,17 @@ const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
 });
 const mesh = new THREE.Mesh(geometry, material);
-
 scene.add(mesh);
 
 // Init Camera
+const camera = new THREE.PerspectiveCamera(75, SIZES.width / SIZES.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(SIZES.width, SIZES.height);
+renderer.render(scene, camera);
