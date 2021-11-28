4f81c5de3bd2020ff1b66651975018fd73c55d0c
Pavel Klochkov <paul.klochkov@gmail.com>
2021-11-28 11:21:41 +0100

    Initial commit

---
+++ 03-basic-scene/script.js
@@ -0,0 +1,13 @@
+// Init scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Init Camera
