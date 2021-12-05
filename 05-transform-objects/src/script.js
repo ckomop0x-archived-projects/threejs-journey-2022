158fa7a5b1d3ed7783ee04b85004fbba7122e79d
Pavel Klochkov <paul.klochkov@gmail.com>
2021-12-05 10:51:55 +0100

    Add camera

---
+++ 05-transform-objects/src/script.js
@@ -0,0 +1,56 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0.5, -0.9, 0.1);
+scene.add(mesh);
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
+
+/**
+ * Scale
+ */
+
+mesh.scale.set(2, 0.25, 0.5);
+mesh.rotation.set(0, 0.25, 0);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(0.5, 0.2, 3);
+scene.add(camera);
+
+camera.lookAt(mesh.position);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
